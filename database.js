88d83b21442003fd469d4fd1f463879e99c03d17
Balaji Kosuri <123826865+Balajikosuri@users.noreply.github.com>
2024-01-08 23:21:48 +0530

    dotenv fun added

--- database.js
+++ database.js
@@ -1,5 +1,6 @@
+const dotenv = require("dotenv");
 const { Pool } = require('pg')
- 
+dotenv.config();
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 })
